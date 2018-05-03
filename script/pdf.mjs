ccffb9daf3417451e3d8bc10480b1f4340b84d91
aratama <aratama@example.com>
2018-05-03 15:05:50 +0900

    use puppeteer for pdf generation

---
+++ script/pdf.mjs
@@ -0,0 +1,19 @@
+import puppeteer from "puppeteer"
+
+async function main(){
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(`file:///${process.cwd()}/dist/purescript-book-ja.html`)
+  await page.pdf({
+    path: "./dist/purescript-book-ja.pdf",
+    format: "A4",
+    printBackground: true,
+    displayHeaderFooter: false,
+    margin: {
+      left: "0.35cm"
+    }
+  })
+  await browser.close()
+}
+
+main()
